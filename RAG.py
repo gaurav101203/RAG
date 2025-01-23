29ddcb75c42dc9e78c945de222284237eb126294
gaurav101203 <162018942+gaurav101203@users.noreply.github.com>
2025-01-23 19:23:52 +0530

    Update RAG.py

--- RAG.py
+++ RAG.py
@@ -24,14 +24,14 @@ Original file is located at
 # Ensure your VertexAI credentials are configured
 import os
 import gradio as gr
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./rag-model-448019-7622d30ebd3b.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./GOOGLE_APPLICATION_CREDENTIALS"
 
 
 from google.oauth2 import service_account
 from google.auth.transport.requests import Request
 
 credentials = service_account.Credentials.from_service_account_file(
-    "./rag-model-448019-7622d30ebd3b.json",
+    "./GOOGLE_APPLICATION_CREDENTIALS",
     scopes=["https://www.googleapis.com/auth/cloud-platform"]
 )
 credentials.refresh(Request())
